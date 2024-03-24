4e68b770156c310653a97f9c301e27e813d25345
Petr Travkin <konorlevich@gmail.com>
2024-03-24 20:25:19 +0400

    add "Median of Two Sorted Arrays"

---
+++ src/median_of_two_sorted_arrays/median_of_two_sorted_arrays.go
@@ -0,0 +1,38 @@
+package median_of_two_sorted_arrays
+
+// findMedianSortedArrays
+//
+// Given two sorted arrays `nums1` and `nums2` of size `m` and `n` respectively,
+// return the median of the two sorted arrays.
+//
+// The overall run time complexity should be `O(log(m+n))`.
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	if len(nums1) == 0 && len(nums2) == 0 {
+		return 0
+	}
+	newNum := make([]int, 0, len(nums1)+len(nums2))
+	i1, i2 := 0, 0
+
+	for i1 <= len(nums1) && i2 <= len(nums2) {
+		if i1 >= len(nums1) {
+			newNum = append(newNum, nums2[i2:]...)
+			break
+		}
+		if i2 >= len(nums2) {
+			newNum = append(newNum, nums1[i1:]...)
+			break
+		}
+		if nums1[i1] > nums2[i2] {
+			newNum = append(newNum, nums2[i2])
+			i2++
+		} else {
+			newNum = append(newNum, nums1[i1])
+			i1++
+		}
+	}
+	halfLen := len(newNum) / 2
+	if len(newNum)%2 == 1 {
+		return float64(newNum[halfLen])
+	}
+	return float64(newNum[halfLen-1]+newNum[halfLen]) / float64(2)
+}
---
+++ src/median_of_two_sorted_arrays/median_of_two_sorted_arrays_test.go
@@ -0,0 +1,35 @@
+package median_of_two_sorted_arrays
+
+import "testing"
+
+func Test_findMedianSortedArrays(t *testing.T) {
+	tests := []struct {
+		name  string
+		nums1 []int
+		nums2 []int
+		want  float64
+	}{
+		{name: "empty"},
+
+		//Explanation: merged array = [1,2,3] and median is 2.
+		{name: "nums1 = [1,3], nums2 = [2]",
+			nums1: []int{1, 3},
+			nums2: []int{2},
+			want:  float64(2),
+		},
+
+		//Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+		{name: "nums1 = [1,2], nums2 = [3,4]",
+			nums1: []int{1, 2},
+			nums2: []int{3, 4},
+			want:  2.50000,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMedianSortedArrays(tt.nums1, tt.nums2); got != tt.want {
+				t.Errorf("findMedianSortedArrays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
