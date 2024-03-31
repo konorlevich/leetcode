d2db874696c332f311835cc5344586740acaf67c
Petr Travkin <konorlevich@gmail.com>
2024-03-31 13:24:01 +0400

    refactor(two sum): remove obsolete wrapper function, simplify tests, add package docs

--- src/two_sum/two_sum.go
+++ src/two_sum/two_sum.go
@@ -1,14 +1,12 @@
-package two_sum
-
-// twoSum
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// Package two_sum
+//
+// Given an array of integers nums and an integer target,
+// return indices of the two numbers such that they add up to target.
 //
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 //
 // You can return the answer in any order.
-func twoSum(f func([]int, int) []int, nums []int, target int) []int {
-	return f(nums, target)
-}
+package two_sum
 
 // bruteForce
 //
