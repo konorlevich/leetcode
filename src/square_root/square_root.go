7bef0aee9b0b8111821eae6b44c18550b1a4379b
Petr Travkin <konorlevich@gmail.com>
2024-03-31 13:49:52 +0400

    docs(square_root):add package doc

--- src/square_root/square_root.go
+++ src/square_root/square_root.go
@@ -1,6 +1,4 @@
-package square_root
-
-// mySqrt
+// Package square_root
 //
 // Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
 // The returned integer should be non-negative as well.
@@ -8,6 +6,8 @@ package square_root
 // You must not use any built-in exponent function or operator.
 //
 // For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+package square_root
+
 func mySqrt(x int) int {
 	if x < 2 {
 		return x
