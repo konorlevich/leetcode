9090e100c8350a2ac86dcba08e9c3feb28d3be22
Petr Travkin <konorlevich@gmail.com>
2024-03-31 14:06:58 +0400

    docs(convert_sorted_array_to_binary_search_tree): add package docs

--- src/convert_sorted_array_to_binary_search_tree/convert_sorted_array_to_binary_search_tree.go
+++ src/convert_sorted_array_to_binary_search_tree/convert_sorted_array_to_binary_search_tree.go
@@ -1,14 +1,14 @@
-package convert_sorted_array_to_binary_search_tree
-
-import "github.com/konorlevich/leetcode/src/common"
-
-// sortedArrayToBST
+// Package convert_sorted_array_to_binary_search_tree
 //
 // Given an integer array nums where the elements are sorted in ascending order,
 // convert it to a height-balanced binary search tree.
 //
 // A height-balanced binary tree
 // is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+package convert_sorted_array_to_binary_search_tree
+
+import "github.com/konorlevich/leetcode/src/common"
+
 func sortedArrayToBST(nums []int) (root *common.TreeNode) {
 	numLen := len(nums)
 	if numLen == 0 {
