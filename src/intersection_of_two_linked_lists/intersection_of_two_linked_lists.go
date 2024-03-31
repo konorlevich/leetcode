80393d1e3a252083b83d256d9c613183ea0b978f
Petr Travkin <konorlevich@gmail.com>
2024-03-31 14:23:41 +0400

    docs(intersection_of_two_linked_lists): add package docs

--- src/intersection_of_two_linked_lists/intersection_of_two_linked_lists.go
+++ src/intersection_of_two_linked_lists/intersection_of_two_linked_lists.go
@@ -1,3 +1,27 @@
+// Package intersection_of_two_linked_lists
+//
+// Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect.
+// If the two linked lists have no intersection at all, return null.
+//
+// The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+//
+// Note that the linked lists must retain their original structure after the function returns.
+//
+// The inputs to the judge are given as follows (your program is not given these inputs):
+//
+// - intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+//
+// - listA - The first linked list.
+//
+// - listB - The second linked list.
+//
+// - skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+//
+// - skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+//
+// The judge will then create the linked structure based on these inputs and pass the two heads,
+// headA and headB to your program.
+// If you correctly return the intersected node, then your solution will be accepted.
 package intersection_of_two_linked_lists
 
 import (
