cc7b6eb78160413eeabf1ab35643e9a16ca4de3e
Petr Travkin <konorlevich@gmail.com>
2024-03-31 13:47:55 +0400

    docs(search_insert_position):add package doc

--- src/search_insert_position/search_insert_position.go
+++ src/search_insert_position/search_insert_position.go
@@ -1,11 +1,11 @@
-package search_insert_position
-
-// searchInsert
+// Package search_insert_position
 //
 // Given a sorted array of distinct integers and a target value, return the index if the target is found.
 // If not, return the index where it would be if it were inserted in order.
 //
 // You must write an algorithm with O(log n) runtime complexity.
+package search_insert_position
+
 func searchInsert(nums []int, target int) int {
 	numLen := len(nums)
 	if numLen == 0 {
