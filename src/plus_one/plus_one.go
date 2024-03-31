ca3d3c68f79ccf15364bec5aaa2bc339f74a251f
Petr Travkin <konorlevich@gmail.com>
2024-03-31 13:48:46 +0400

    docs(plus_one):add package doc

--- src/plus_one/plus_one.go
+++ src/plus_one/plus_one.go
@@ -1,12 +1,12 @@
-package plus_one
-
-// plusOne
+// Package plus_one
 //
 // You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
 // The digits are ordered from most significant to least significant in left-to-right order.
 // The large integer does not contain any leading 0's.
 //
 // Increment the large integer by one and return the resulting array of digits.
+package plus_one
+
 func plusOne(digits []int) []int {
 	lastIndex := len(digits) - 1
 	if lastIndex < 0 {
