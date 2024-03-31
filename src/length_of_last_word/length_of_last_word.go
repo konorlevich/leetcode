f8df1d302f579248c948660fd118d1af73f8eff2
Petr Travkin <konorlevich@gmail.com>
2024-03-31 13:48:21 +0400

    docs(length_of_last_word):add package doc

--- src/length_of_last_word/length_of_last_word.go
+++ src/length_of_last_word/length_of_last_word.go
@@ -1,13 +1,13 @@
-package length_of_last_word
-
-import "strings"
-
-// lengthOfLastWord
+// Package length_of_last_word
 //
 // Given a string s consisting of words and spaces,
 // return the length of the last word in the string.
 //
 // A word is a maximal substring consisting of non-space characters only.
+package length_of_last_word
+
+import "strings"
+
 func lengthOfLastWord(s string) int {
 	lastIndex := max(len(strings.TrimRight(s, " "))-1, 0)
 	for i := lastIndex; i >= 0; i-- {
