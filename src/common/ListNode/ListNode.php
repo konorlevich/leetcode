881fbfa79c2539d73f631a020e042c32f95ad29e
Petr Travkin <konorlevich@gmail.com>
2024-04-02 21:45:13 +0400

    feat: add ListNode class with ListNode::fromList method

---
+++ src/common/ListNode/ListNode.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Konorlevich\Leetcode\common\ListNode;
+
+class ListNode
+{
+    public int $val = 0;
+    public ?ListNode $next = null;
+
+    function __construct(int $val = 0, ?ListNode $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+
+    /**
+     * @param Integer[] $list
+     * @return ?ListNode
+     */
+    public static function fromList(array $list): ?ListNode
+    {
+        $node = null;
+        for ($i = count($list)-1; $i >= 0 ; $i--) {
+            $node = new ListNode($list[$i], $node);
+        }
+        return $node;
+    }
+}
---
+++ tests/common/ListNode/ListNodeTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace common\ListNode;
+
+use Konorlevich\Leetcode\common\ListNode\ListNode;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(ListNode::class)]class ListNodeTest extends TestCase
+{
+    public static function dataProvider(): array
+    {
+        return [
+            'empty' => [
+                'list'=> [],
+                'expected'=> null,
+            ],
+            '[1,2,3,4]' => [
+                'list'=> [1,2,3,4],
+                'expected'=> new ListNode(1,
+                    new ListNode(2,
+                    new ListNode(3,
+                    new ListNode(4)
+                    ))),
+            ],
+            '[4,3,2,1]' => [
+                'list'=> [4,3,2,1],
+                'expected'=> new ListNode(4,
+                    new ListNode(3,
+                    new ListNode(2,
+                    new ListNode(1)
+                    ))),
+            ],
+        ];
+    }
+
+    /**
+     * @param array $list
+     * @param ListNode|null $expected
+     * @return void
+     */
+    #[DataProvider('dataProvider')] public function testFromList(
+        array $list,
+        ?ListNode $expected,
+    )
+    {
+        $this->assertEquals($expected, ListNode::fromList($list));
+    }
+}
